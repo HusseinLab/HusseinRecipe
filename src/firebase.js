cc7e75850a9db58f46a0a579499190b3507c2df8
HusseinLab <husseinhajjhassan91@gmail.com>
2025-06-26 21:47:11 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,15 @@ import { getFirestore }    from "https://www.gstatic.com/firebasejs/11.6.1/fireb
 import { getStorage }      from "https://www.gstatic.com/firebasejs/11.6.1/firebase-storage.js";
 import { marked }          from "https://cdn.jsdelivr.net/npm/marked@4.3.0/lib/marked.esm.js";
 
-const firebaseConfig = { /* …as before… */ };
+const firebaseConfig = {
+        apiKey: "AIzaSyBiV-BFHLVy0EbKIl9gnt2j-QsLUyvkZvs",
+        authDomain: "my-personal-recipe-book-8b55d.firebaseapp.com",
+        projectId: "my-personal-recipe-book-8b55d",
+        storageBucket: "my-personal-recipe-book-8b55d.firebasestorage.app",
+        messagingSenderId: "932879383972",
+        appId: "1:932879383972:web:aa977406634fa061485531",
+        measurementId: "G-ZWP1BKDXY4"
+};
 const app     = initializeApp(firebaseConfig);
 const auth    = getAuth(app);
 const db      = getFirestore(app);
